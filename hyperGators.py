664a188c4c12018f2b092e5ed107f91cb9688c3d
Ka-faldu <kfaldu@ufl.edu>
2024-02-24 12:11:18 -0500

    intro message

--- hyperGators.py
+++ hyperGators.py
@@ -1,5 +1,5 @@
 #Karan, Jose
-import math
+
 
 def main():
     points=0
@@ -12,7 +12,42 @@ def main():
     print("We hired you to bring back unobtainium from the end of the Black Hole.")
     print("The universsssal council doesn't like ussss trying to obtain that material.")
     print("Ya know... on account of it being unobtainium and all.")
-    print("Anywayssss, that's why we hired you.... What was your name again? ")
+    print("Anywayssss, that's why we hired you.... What was your name again? \n")
+    print("Enter player name: ", end ='')
     player_name=input()
-    print(f'ah yes, the infamous {player_name}')
+    print(f'Ah yes, the infamoussss Gator Pirate {player_name} \n')
+    print("Next time you pop around, I'll have a sssshop open for you")
+    print("You don't have any money?")
+    print("...")
+    print("...")
+    print("Well if you find any unobtainium sssshardssss in there, you can buy my waressss with those")
+    print("Go on then! Go get me some unobtainium\n")
 
+    print("Start Menu")
+    print("----------")
+    print("1. Start Run")
+    print("2. Shop")
+    print("3. Exit")
+    print("\nEnter selection number: ", end ='')
+    menu_option=input()
+    while not menu_option.isnumeric():
+            print("Invalid input, please enter an integer from the menu selection: ", end = '')
+            menu_option=input()
+    menu_option = int(menu_option)
+    if menu_option==3:
+        print("\nGoodbye! Please come work for HyperGator again!", end='')
+    while(menu_option != 3):
+        match menu_option:
+            case 1:
+                pass
+            case 2:
+                openShop(points)
+            case 3:
+                continue
+
+def openShop(shards):
+    print("Welcome to the HyperGator sssshop")
+
+
+if __name__ == "__main__":
+    main()
