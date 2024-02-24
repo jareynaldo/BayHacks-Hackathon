9743027ae5f9f073ee47f202140aad5b7f1b1ed2
Ka-faldu <kfaldu@ufl.edu>
2024-02-24 12:08:13 -0500

    adding program start

---
+++ hyperGators.py
@@ -0,0 +1,18 @@
+#Karan, Jose
+import math
+
+def main():
+    points=0
+    attack=10
+    defense=10
+    health=10
+    speed=10
+    skills=["Single", "-", "-"]
+    print("Welcome to Hyper Gator!\n")
+    print("We hired you to bring back unobtainium from the end of the Black Hole.")
+    print("The universsssal council doesn't like ussss trying to obtain that material.")
+    print("Ya know... on account of it being unobtainium and all.")
+    print("Anywayssss, that's why we hired you.... What was your name again? ")
+    player_name=input()
+    print(f'ah yes, the infamous {player_name}')
+
