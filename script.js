78110eca21e139656985f5f7317d44a0387ee24f
José Reynaldo <ja.reynaldo@outlook.com>
2024-02-24 22:29:23 -0500

    will have to wake up at 5am

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 let displayingData = document.getElementById('currently-displaying');
+let gameStats = document.getElementById('game-stats');
+
 let tittle  = document.getElementById('Tittle');
+const bottomSection = document.getElementById('bottom-section')
+const bottomClass = document.getElementsByClassName('bottom')
 const btn = document.getElementsByTagName('button');
 
-console.log("yellor");
 
 [...btn].forEach(nexty => {
     nexty.addEventListener("click", () => {
-
-        // Remove any existing content
+        // clear out display, create new div element, set the content inside the 
+        // new div, and append to a particlar element
         displayingData.innerHTML = "";
+        let gameOutput = document.createElement('div');
+        gameOutput.id = 'gameOutput';  
+        displayingData.append(gameOutput);
 
-        // Add the image with the 'blackhole' ID
+let newText = document.createElement('div');
+newText.className = 'putting-text';   
+displayingData.appendChild(newText);
+
         displayingData.innerHTML = '<img id="blackhole" src="images/Black_hole_representation.gif" alt="This is a gif of a blackhole"/>';
 
-        // Add the 'show' class to trigger the animation
+
+
+
         displayingData.classList.add('show');
 
-        tittle.style.animation = "leaveQuietly 5s ease forwards";
-        setTimeout(() =>  tittle.innerHTML = "" , 5000); 
+        tittle.style.animation = "leaveQuietly 2s ease-out forwards";
+        console.log(bottomSection);
+        bottomSection.style.display = "flex";
+    
+        setTimeout(() =>  { tittle.remove();
+            tittle.remove()
+            bottomSection.style.backgroundImage = "url('images/console.jpeg')";
 
+        } , 5000);
+
+        let yuhsuh = document.createElement('p');
+        yuhsuh.className = "text-output"
+        displayingData.append(yuhsuh);
     });
 });
+
+
